0900c95aa7ee9dd115e544e5340954b79252a119
kminny <darkdevil94@naver.com>
2021-08-31 02:19:55 +0900

    Docker added for redis adapter & socket broadcast

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -7,12 +7,19 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
+import { from, map, Observable } from 'rxjs';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class EventsGateway implements OnGatewayInit {
   @WebSocketServer()
   server: Server;
+
+  @SubscribeMessage('connection')
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log(client);
+    client.broadcast.emit('events', 'I am here!!!');
+  }
 
   @SubscribeMessage('events')
   handleEvent(
@@ -23,6 +30,13 @@ export class EventsGateway implements OnGatewayInit {
     console.log(data);
     console.log(client);
     return { event, data };
+  }
+
+  @SubscribeMessage('identity')
+  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    return from([1, 2, 3]).pipe(
+      map((item) => ({ event: 'events', data: item })),
+    );
   }
 
   afterInit(server: Server) {
